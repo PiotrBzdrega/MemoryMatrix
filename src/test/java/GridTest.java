207db60c93f644f65a00f73fa3c677f7d8769089
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-01-14 22:01:06 +0100

    modified order of calls in Junit test

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -9,12 +9,14 @@ class GridTest {
     @Test
     void amountSelectedElementsAndRandomnessCheck() {
         Grid grid =new Grid(10);
-        ArrayList array=new ArrayList();
+        //ArrayList array=new ArrayList();
         for (int i = 0; i<1000;i++){
-            assertEquals(10, grid.getCoveredCells().size());
-            array=grid.getCoveredCells();
-            grid.deselectAllSelected();
+
+            //array=grid.getCoveredCells();
+
             grid.createPuzzle();
+            assertEquals(10, grid.getCoveredCells().size());
+            grid.deselectAllSelected();
             //assertNotEquals(array,grid.getCoveredCells());
             //System.out.println(grid.getCoveredCells());
         }
