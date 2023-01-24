e1a5dbd024f6e608e8c3c43e23c017f0b659e604
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-01-24 23:47:31 +0100

    Main class to call puzzle

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,23 @@
 public class Game {
     private Grid grid;
+    private Gui gui;
 
-    public Game(int length) {
-        this.grid =new Grid(length);
+    public Game(Grid grid, Gui gui) {
+        this.grid = grid;
+        this.gui = gui;
+    }
+
+    private void initializeCanvas (){
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Grid grid =new Grid(10);
+        Gui gui=new Gui(grid.getGrid(),grid.getxRows());
+        Game game=new Game(grid,gui);
+   //     grid.createPuzzle();
     }
 }
