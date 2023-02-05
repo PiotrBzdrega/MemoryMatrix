2fa8144a26bcfe71fbbf40a6fd6b69f59e648e31
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-02-05 11:12:57 +0100

    narrow test case

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,21 +1,49 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GridTest {
 
-    @Test
-    void amountSelectedElementsAndRandomnessCheck() {
-        Grid grid =new Grid(10);
-        grid.changeCoveredElements(10);
-        for (int i = 0; i<1000;i++){
+    Grid grid = new Grid(10);
+    Gui gui = new Gui(grid.getGrid(), grid.getxRows());
+    Game game = new Game(grid, gui);
 
+    @Test
+    void randomnessCheck() {
+        int elem = 1000;
+        MySQLTest sql = new MySQLTest("jdbc:mysql://localhost:3306/junit", "root", "admin");
+
+        sql.deleteTable("randtest");
+        grid.changeCoveredElements(10);
+        ArrayList<ArrayList<Integer>> dataBaseArray = new ArrayList<>();
+        for (int i = 0; i < elem; i++) {
             grid.createPuzzle();
-            assertEquals(10, grid.getCoveredCells().size());
+            System.out.println("inserted " + i);
+            sql.insertArr("randtest", i, "idx", "cell", grid.getCoveredCells());
             grid.deselectAllSelected();
         }
+
+        for (int i = 0; i < elem; i++) {
+            dataBaseArray.add(sql.selectArr("randtest", i, "idx", "cell"));
+        }
+
+        for (int i = 0; i < elem; i++) {
+            if (i > 0) {
+                System.out.println("compared " + i);
+                for (int j = i - 1; j > 0; j--) {
+                    assertNotEquals(dataBaseArray.get(i), dataBaseArray.get(j), "Random function created same sample after " + i + " draw");
+                }
+            }
+
+
+        }
+
+
+        sql.close();
 
     }
 
