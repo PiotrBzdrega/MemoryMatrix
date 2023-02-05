f871356cd763b2df68a281aed90ed5525f3497be
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-02-05 12:37:23 +0100

    startChapter method added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.*;
+
 public class Game {
     private Grid grid;
     private Gui gui;
@@ -7,17 +14,29 @@ public class Game {
         this.gui = gui;
     }
 
-    private void initializeCanvas (){
+    public void startChapter() {
+        grid.createPuzzle();
+        grid.changeState(true);
+        gui.refreshInterface();
+        //gui.freezeGrid()
+    }
 
+    public void startGuessing() {
+        grid.changeState(false);
+        gui.refreshInterface();
+    }
 
+    public void freezeGrid() {
 
     }
 
     public static void main(String[] args) {
 
-        Grid grid =new Grid(10);
-        Gui gui=new Gui(grid.getGrid(),grid.getxRows());
-        Game game=new Game(grid,gui);
-   //     grid.createPuzzle();
+        Grid grid = new Grid(5);
+        Gui gui = new Gui(grid.getGrid(), grid.getxRows());
+        Game game = new Game(grid, gui);
+        grid.changeCoveredElements(2);
+        game.startChapter();
+        //   grid.createPuzzle();
     }
 }
