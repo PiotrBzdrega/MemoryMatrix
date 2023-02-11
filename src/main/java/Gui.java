813963b100a815be8c035c0e6004065c27f5e27e
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-02-11 07:00:29 +0100

    GridBagLayout instead BoarderLayout

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -4,78 +4,101 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Gui implements ActionListener  {
-
-    JLabel label;
+public class Gui implements ActionListener {
 
     JFrame frame;
-    JPanel panel;
     private ArrayList<JButton> button;
 
     ArrayList<Cell> grid;
 
-    public Gui(ArrayList<Cell> grid, int size){
-        this.grid=grid;
-        createPanel(size);
-        createButtons(size);
+    public Gui(ArrayList<Cell> grid, int size) {
+        this.grid = grid;
+
         createFrame();
-
-/*        button = new JButton("adamek");
-        button.addActionListener(this); //when button to be pressed, actionPerformed() will be called
-        label=new JLabel("Number of clicks");
-
-    frame=new JFrame();
-    panel=new JPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-    panel.setLayout(new  GridLayout(5,5));
-    panel.add(button);
-    panel.add(label);
-
-        frame.add(panel,BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setTitle("Title");
-    frame.pack();
-    frame.setVisible(true);*/
-}
-
-    private void createPanel(int dimension){
-        panel=new JPanel();
-        panel.setBounds(0,0,500,500);
-        panel.setLayout(new GridLayout(dimension,dimension));
-    }
-    private void createFrame(){
-        frame=new JFrame();
-        frame.setSize(800,800);
-        //frame.getContentPane().add(panel);
-        frame.add(panel,null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Memory Matrix");
+        createButtons(size);
         frame.pack();
-        frame.setVisible(true);
     }
+
+
+    private void createFrame() {
+        frame = new JFrame("GridBagLayoutDemo");
+        frame.setBounds(100, 100, 1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new GridBagLayout());
+        frame.setTitle("Memory Matrix");
+        frame.getContentPane().setBackground(new Color(50, 200, 92));
+
+        frame.setVisible(true);
+
+
+        /* Remove toolbar (close,maximize,minimize,stretch window)
+        frame.setUndecorated(true);
+        frame.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+         */
+    }
+
+
     /**
      * initialize
      */
-    private void createButtons(int size){
-        this.button=new ArrayList<>();
+    private void createButtons(int size) {
+        this.button = new ArrayList<>();
         JButton tempButton;
-        for (int i=0;i<(size*size);i++){
-            tempButton=new JButton();
-            tempButton.setBackground(this.grid.get(i).getColor());
-            tempButton.addActionListener(this);
-            tempButton.setSize(50,50);
-            //tempButton.setContentAreaFilled(false);
-            button.add(tempButton);
-            panel.add(tempButton);
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                tempButton = new JButton();
+                tempButton.setBackground(this.grid.get(i).getColor());
+                tempButton.setEnabled(false);
+                tempButton.setSize(50, 50);
+                //tempButton.setContentAreaFilled(false);
+                button.add(tempButton);
+                //tempButton.addActionListener(this);
+                c.gridx = j;
+                c.gridy = i;
+                frame.getContentPane().add(tempButton, c);
+            }
         }
+
+        tempButton = new JButton("Start");
+        tempButton.setName("Start");
+        tempButton.addActionListener(this);
+        c.fill = GridBagConstraints.BOTH;
+        c.ipady = 0;       //reset to default
+        c.weightx = 1.0;   //request any extra vertical space
+        c.weighty = 1.0;   //request any extra vertical space
+        //c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+        c.insets = new Insets(5, 0, 5, 0);  //top padding
+        c.gridx = size / 2;       //aligned with button 2
+        c.gridwidth = 2;   //2 columns wide
+        c.gridy = size + 1;       //third row
+        frame.getContentPane().add(tempButton, c);
+
+        //JLabel label = new JLabel("Number of clicks");
+        //frame.getContentPane().add(label);
     }
-    public void toggleListener(){
-        for (JButton btn: button) {
-            if (btn.getAction()!=null)
+
+    private void toggleListener() {
+        for (JButton btn : button) {
+            if (btn.getAction() != null)
                 btn.addActionListener(this);
-            else{
+            else {
                 btn.removeActionListener(this);
                 System.out.println("removed Listener");
+            }
+        }
+    }
+
+    private void toggleEnable() {
+        for (JButton btn : button) {
+            if (btn.isEnabled()) {
+                btn.setEnabled(false);
+            } else {
+                btn.setEnabled(true);
             }
 
         }
@@ -84,26 +107,37 @@ public class Gui implements ActionListener  {
     /**
      * pop up with set message
      */
-    private void callMessage(String message){
-      //  if(eve.getSource() == jb1)
-            JOptionPane.showMessageDialog(panel, message);
+    private void callMessage(String message) {
+        //  if(eve.getSource() == jb1)
+        JOptionPane.showMessageDialog(frame, message);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        int i=button.indexOf(e.getSource());
-        this.grid.get(i).lightUp();
-        button.get(i).setBackground(this.grid.get(i).getColor());
-    }
-    public void refreshInterface(){
-        for (int i=0;i< button.size();i++) {
+        //System.out.println(e.getSource());
+        if (e.getSource() instanceof JButton) {
+            JButton j = (JButton) e.getSource();
+            System.out.println();
 
-            if (this.grid.get(i).isState()) {
-                System.out.println("hei");
+            if (j.getName().equals("Start"))
+                j.setVisible(false);
+            else {
+                int i = button.indexOf(e.getSource());
+                this.grid.get(i).lightUp();
                 button.get(i).setBackground(this.grid.get(i).getColor());
-                System.out.println(i);
             }
         }
-            }
+
+
     }
+
+    public void refreshInterface() {
+        for (int i = 0; i < button.size(); i++) {
+
+            if (this.grid.get(i).isState()) {
+                button.get(i).setBackground(this.grid.get(i).getColor());
+            }
+        }
+    }
+}
 
