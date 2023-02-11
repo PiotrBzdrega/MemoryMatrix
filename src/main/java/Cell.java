d7fae7d0267186f012c59d52d7068527222d5f49
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-02-11 06:58:20 +0100

    color squeeze implementation

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -2,21 +2,12 @@ import java.awt.*;
 
 public class Cell {
 
-    private final int xDim;
-    private final int yDim;
     private Color[] color;
     private boolean state;
-
-
-
     private boolean selected;
 
-    public Cell(int xDim, int yDim) {
-        this.xDim = xDim;
-        this.yDim = yDim;
-        this.color = new Color[2];
-        this.color[0] = Color.white;
-        this.color[1] = Color.red;
+    public Cell() {
+        this.color = new Color[]{Color.white, Color.red};
         this.state = false;
         this.selected = false;
     }
@@ -48,7 +39,7 @@ public class Cell {
         this.state = false;
     }
 
-    public Color getColor(){
+    public Color getColor() {
         return (this.state ? this.color[1] : this.color[0]);
     }
 
